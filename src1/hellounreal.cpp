8ffad356f719bf314320b6ed34fbcda337ae2a37
Miha Stele <stele1000@gmail.com>
2021-03-06 17:58:52 +0100

    init

--- src1/hellounreal.cpp
+++ src1/hellounreal.cpp
@@ -17,30 +17,28 @@ bool guessed_numbers(int *expected, int *actual)
         bool found = false;
         for (size_t j = 0; j < 3; j++)
         {
-            cout << "TEST EXPECTED ACTUAL" << " " << expected[i] << " == " << actual[j] << endl;
+            cout << "TEST EXPECTED ACTUAL"
+                 << " " << expected[i] << " == " << actual[j] << endl;
             if (expected[i] == actual[j] && expected[i] != INT_MIN && !found) // check if same values and not null
             {
                 expected[i] = INT_MIN;
                 actual[j] = INT_MIN;
                 found = true;
             }
-            
         }
     }
 
     for (size_t i = 0; i < (sizeof(expected) / sizeof(*expected)); i++)
     {
         cout << "TESTTTT " << expected[0] << " "
-         << expected[1] << " " << expected[2] << endl;
+             << expected[1] << " " << expected[2] << endl;
 
         if (expected[i] != INT_MIN)
         {
             cout << "You have guess wrong and now you died...";
             return true;
         }
-        
     }
-    
 
     return false;
 }
@@ -49,20 +47,22 @@ bool game_turn()
 {
     string string;
 
-    int number1 = 3;
-    int number2 = 3;
-    int number3 = 3;
-    int sum = number1 + number2 + number3;
+    int number1 = rand() % 5 + 1;
+    int number2 = rand() % 5 + 1;
+    int number3 = rand() % 5 + 1;
+    cout << "DEBUG " << number1 << " " << number2 << " " << number3 << " " << endl;
+    long sum = number1 + number2 + number3;
     long mul = number1 * number2 * number3;
 
     cout << "the three numebrs together are summed up to " << sum << " and multiplied to " << mul << endl;
-    cout << "Guess the numbers, write them with spaces and with x in the end" << endl;
+    cout << "Guess the numbers, write them with spaces followed by at least a space" << endl;
     cin >> string;
 
     int guessed1, guessed2, guessed3;
     guessed1 = stoi(string.substr(0, string.find(" ")));
-    guessed2 = stoi(string.substr(0, string.find(" ")));
-    guessed3 = stoi(string.substr(0, string.find(" ")));
+    guessed2 = stoi(string.substr(1, string.find(" ")));
+    guessed3 = stoi(string.substr(2, string.find(" ")));
+    cout << "DEBUG " << guessed1 << " " << guessed2 << " " << guessed3 << " " << endl;
 
     // cout << "Your guess are the following numbers " << guessed1 << " "
     // << guessed2 << " " << guessed3 << endl;
