69585c31eea448dd1c8d1107722022f67e0ed72e
Miha Stele <stele1000@gmail.com>
2021-03-07 07:27:07 +0100

    Fix the game logic

--- src1/hellounreal.cpp
+++ src1/hellounreal.cpp
@@ -3,22 +3,24 @@
 
 using namespace std;
 
+const short GUESS_SIZE = 3;
+
 bool guessed_numbers(int *expected, int *actual)
 {
 
-    cout << "Your guess are the following numbers " << expected[0] << " "
+    cout << "The actual numbers were " << expected[0] << " "
          << expected[1] << " " << expected[2] << endl;
 
-    cout << "The actual numbers were " << actual[0] << " "
+    cout << "Your guessed numbers were " << actual[0] << " "
          << actual[1] << " " << actual[2] << endl;
 
-    for (size_t i = 0; i < 3; i++)
+    for (size_t i = 0; i < GUESS_SIZE; i++)
     {
         bool found = false;
-        for (size_t j = 0; j < 3; j++)
+        for (size_t j = 0; j < GUESS_SIZE; j++)
         {
-            cout << "TEST EXPECTED ACTUAL"
-                 << " " << expected[i] << " == " << actual[j] << endl;
+            /* cout << "TEST EXPECTED == ACTUAL"
+                 << " " << expected[i] << " == " << actual[j] << endl; */
             if (expected[i] == actual[j] && expected[i] != INT_MIN && !found) // check if same values and not null
             {
                 expected[i] = INT_MIN;
@@ -28,10 +30,10 @@ bool guessed_numbers(int *expected, int *actual)
         }
     }
 
-    for (size_t i = 0; i < (sizeof(expected) / sizeof(*expected)); i++)
+    for (size_t i = 0; i < GUESS_SIZE; i++)
     {
-        cout << "TESTTTT " << expected[0] << " "
-             << expected[1] << " " << expected[2] << endl;
+        /* cout << "DEBUG " << expected[0] << " "
+             << expected[1] << " " << expected[2] << endl; */
 
         if (expected[i] != INT_MIN)
         {
@@ -45,8 +47,6 @@ bool guessed_numbers(int *expected, int *actual)
 
 bool game_turn()
 {
-    string str;
-
     int number1 = rand() % 5 + 1;
     int number2 = rand() % 5 + 1;
     int number3 = rand() % 5 + 1;
@@ -55,19 +55,13 @@ bool game_turn()
     long mul = number1 * number2 * number3;
 
     cout << "the three numebrs together are summed up to " << sum << " and multiplied to " << mul << endl;
-    cout << "Guess the numbers, write them with spaces followed by at least a space" << endl;
-    cin >> str;
+    cout << "Guess the numbers, write them with spaces" << endl;
 
     int guessed1, guessed2, guessed3;
-    int pos = 0;
 
-    string del = " ";
-    guessed1 = stoi(str.substr(0, str.find(" ", pos)));
-    str.erase(0, pos + del.length());
-    guessed2 = stoi(str.substr(0, str.find(" ", pos)));
-    str.erase(0, pos + del.length());
-    guessed3 = stoi(str.substr(0, str.find(" ", pos)));
-    str.erase(0, pos + del.length());
+    cin >> guessed1;
+    cin >> guessed2;
+    cin >> guessed3;
 
     // cout << "Your guess are the following numbers " << guessed1 << " "
     // << guessed2 << " " << guessed3 << endl;
